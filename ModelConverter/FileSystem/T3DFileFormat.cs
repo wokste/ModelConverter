041fb756277f3f502c9b7009b9b25995fb28bfea
Steven Wokke <wokste@users.noreply.github.com>
2015-11-14 12:47:58 +0100

    Fix #5

--- ModelConverter/FileSystem/T3DFileFormat.cs
+++ ModelConverter/FileSystem/T3DFileFormat.cs
@@ -8,6 +8,34 @@ using ModelConverter.DataStructures;
 namespace ModelConverter.FileSystem {
 	class T3DFileFormat {
 		internal static void save(Model m, StreamWriter w) {
+			w.WriteLine("Begin Map");
+			w.WriteLine("Begin Actor Class=LevelInfo Name=LevelInfo2");
+			w.WriteLine("    TimeSeconds=5.440500");
+			w.WriteLine("    AIProfile(0)=148");
+			w.WriteLine("    Level=LevelInfo'MyLevel.LevelInfo2'");
+			w.WriteLine("    Tag=LevelInfo");
+			w.WriteLine("    Region=(Zone=LevelInfo'MyLevel.LevelInfo2',iLeaf=-1)");
+			w.WriteLine("    Name=LevelInfo2");
+			w.WriteLine("End Actor");
+			w.WriteLine("Begin Actor Class=Brush Name=Brush2");
+			w.WriteLine("    MainScale=(SheerAxis=SHEER_ZX)");
+			w.WriteLine("    PostScale=(SheerAxis=SHEER_ZX)");
+			w.WriteLine("    Group=Cube");
+			w.WriteLine("    Level=LevelInfo'MyLevel.LevelInfo2'");
+			w.WriteLine("    Tag=Brush");
+			w.WriteLine("    Region=(Zone=LevelInfo'MyLevel.LevelInfo2',iLeaf=-1)");
+			w.WriteLine("    Begin Brush Name=Brush");
+
+			savePolyList(m, w);
+
+			w.WriteLine("    End Brush");
+			w.WriteLine("    Brush=Model'MyLevel.Model4'");
+			w.WriteLine("    Name=Brush3");
+			w.WriteLine("End Actor");
+			w.WriteLine("End Map");
+		}
+
+		private static void savePolyList(Model m, StreamWriter w) {
 			w.WriteLine("Begin PolyList");
 
 			foreach (var p in m.polygons) {
@@ -43,8 +71,12 @@ namespace ModelConverter.FileSystem {
 
 			while(!r.EndOfStream) {
 				string line = r.ReadLine();
-				if (line.Contains("Begin Polygon"))
+				if (line.Contains("Begin Polygon")){
 					loadPolygon(m, r);
+				} else if (line.Contains("End Polylist")) {
+					// Quickfix to solve T3D files which contain more than one polygon.
+					break;
+				}
 			}
 			return m;
 		}
